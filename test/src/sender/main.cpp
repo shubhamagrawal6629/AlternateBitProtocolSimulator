a6c9a37e80d0786ecec7460451c08fbb32f87668
shubhamagrawal6629 <47285434+shubhamagrawal6629@users.noreply.github.com>
2019-08-16 11:48:46 -0400

    Updated file path using softcoding method

--- test/src/sender/main.cpp
+++ test/src/sender/main.cpp
@@ -21,6 +21,8 @@
 
 #include "../../../include/sender_cadmium.hpp"
 
+#define SENDER_OUTPUTFILE_PATH "../test/data/sender/sender_test_output.txt"
+#define SENDER_INPUTFILE_PATH "../test/data/sender/sender_input_test_control_In.txt"
 using namespace std;
 
 using hclock = chrono::high_resolution_clock;
@@ -55,8 +57,7 @@ int main() {
     auto start = hclock::now(); //to measure simulation execution time
 
     /*************** Loggers *******************/
-    static std::ofstream out_data(
-        "../test/data/sender/sender_test_output.txt");
+    static std::ofstream out_data(SENDER_OUTPUTFILE_PATH);
     struct oss_sink_provider {
         static std::ostream& sink() {
             return out_data;
@@ -105,8 +106,7 @@ int main() {
     /********************************************/
     /****** APPLICATION GENERATOR *******************/
     /********************************************/
-    string input_data_control =
-        "../test/data/sender/sender_input_test_control_In.txt";
+    string input_data_control = SENDER_INPUTFILE_PATH;
     const char* i_input_data_control = input_data_control.c_str();
 
     std::shared_ptr<cadmium::dynamic::modeling::model>generator_con =
